fc60ab4e7f8257c174dd4aeb22f90d6a0888caed
Veronica Bustabad <vbustabad@gmail.com>
2018-10-12 19:58:05 +0000

    automatically backed up by learn

---
+++ src/components/FillerText.js
@@ -0,0 +1,12 @@
+class FillerText extends Component {
+
+  currentTime = () => new Date().toString()
+
+  render() {
+    return (
+      <p>
+        I am a filler text. I can be used to fill your screen. Amazing!
+      </p>
+    );
+  }
+}

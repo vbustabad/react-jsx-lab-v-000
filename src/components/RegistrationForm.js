a376662a62e2939a21dff1de214c81ec08ab6739
Veronica Bustabad <vbustabad@gmail.com>
2018-10-12 19:56:43 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,16 @@
+class RegistrationForm extends Component {
+ 
+  currentTime = () => new Date().toString()
+ 
+  render() {
+    return (
+      <form>
+      </form>
+      <input>
+      </input>
+      <input>
+      </input>
+      <button>
+      </button>
+    );
+  }
